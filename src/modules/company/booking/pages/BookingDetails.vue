eed8426960df2a35cf650106c8eca799e1e66f73
phetaibtc <phetaibtc@gmail.com>
2025-10-13 20:07:55 +0700

    update

--- src/modules/company/booking/pages/BookingDetails.vue
+++ src/modules/company/booking/pages/BookingDetails.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="Booking">
         <a-row :gutter="[0, 16]">
-            <a-col :span="24" v-for="item in Booking.details">
+            <a-col :span="24" v-for="item in Booking.details" v-if="Booking.payment.status === 'success'">
                 <div class=" flex justify-between m-2 shadow p-2">
                     <a-qrcode :value="String(item.id)" />
                     <div>
@@ -12,13 +12,9 @@
                         <h1 class="font-bold">ລາຄາ: {{ Booking.concert.price }}</h1>
                         <h1 class="font-bold">{{ $t('Show_time') }}: {{ Booking.concert.startTime }}-{{
                             Booking.concert.endTime }}</h1>
-                        <!-- <h1 class="font-bold">{{ $t('Show_time') }}: {{ Booking.concert.startTime }}-{{
-                            Booking.concert.endTime }}</h1> -->
                         <a-tag :color="item.status === 'not_checked_in' ? 'green' : 'red'" class="text-center">
                             {{ $t(item.status) }}
                         </a-tag>
-                        <!-- <h1 class="text-lg font-bold">ເລກທີ: {{ item.code }}</h1>
-                        <h1 class="text-lg font-bold">ສະຖານະ: {{ item.status }}</h1> -->
                     </div>
                 </div>
             </a-col>
