93cc6d3c69157af758f7d2a2436e635667bd70a0
phetaibtc <phetaibtc@gmail.com>
2025-10-14 23:56:30 +0700

    update

--- src/modules/company/booking/pages/BookingDetails.vue
+++ src/modules/company/booking/pages/BookingDetails.vue
@@ -1,5 +1,33 @@
 <template>
+    <a-card class="m-1" :loading="loadingBooking" v-if="loadingBooking">whatever content</a-card>
     <div v-if="Booking">
+        <div class="flex justify-between items-center rounded-xl flex-col overflow-hidden m-1"
+            style="box-shadow: 0px 2px 10px #8A959e;">
+            <div class="p-4 w-full flex flex-col gap-2">
+                <div class="flex items-center justify-between w-full">
+                    <div class="text-xl font-bold">
+                        ລາຍການຈອງ : <a-tag color="blue">#{{ Booking.id }}</a-tag>
+                    </div>
+                    <a-tag
+                        :color="Booking.payment.status === 'pending' ? 'gold' : Booking.payment.status === 'success' ? 'green' : 'red'">{{
+                            $t(Booking.payment.status) }}</a-tag>
+                </div>
+                <div class=" bg-blue-100 w-full p-2 my-2 rounded-lg border border-blue-300">
+                    <h1 style="font-weight: bold;">ຂໍ້ມູນການສະແດງ</h1>
+                    <h1>ວັນທີ: {{ dayjs(Booking.concert.date).format('DD-MM-YYYY') }}</h1>
+                    <h1>ເວລາຈັດສະແດງ : {{ Booking.concert.startTime }} - {{ Booking.concert.endTime }}</h1>
+                </div>
+                <div class="flex items-center justify-between w-full">
+                    <div>
+                        <h1>ລາຄາ: {{ Booking.concert.price.toLocaleString() }} kip/{{ $t('seat') }}</h1>
+                        <h1>ຈຳນວນທີ່ຈອງ: {{ Booking.ticket_quantity }} {{ $t('seat') }}</h1>
+                    </div>
+                    <a-tag color="green" style="padding: 10px;font-weight: bold;">
+                        ລວມ: {{ (Booking.total_amount).toLocaleString() }} kip
+                    </a-tag>
+                </div>
+            </div>
+        </div>
         <a-row :gutter="[0, 16]">
             <a-col :span="24" v-for="item in Booking.details" v-if="Booking.payment.status === 'success'">
                 <div class=" flex justify-between m-2 shadow p-2">
@@ -26,7 +54,8 @@
 import { useRoute } from 'vue-router';
 import { onMounted } from 'vue';
 import { useBooking } from '../composables/useBooking';
-const { Booking, fetchBooking } = useBooking()
+import dayjs from 'dayjs';
+const { Booking, fetchBooking, loadingBooking } = useBooking()
 const route = useRoute()
 const bookingId = route.params.id
 
